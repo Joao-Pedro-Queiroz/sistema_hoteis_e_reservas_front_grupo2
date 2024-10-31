e8de89fcac58c6416dd01b6316f46e970e3f484a
pedrofardin <pedro.fardin@hotmail.com>
2024-10-31 11:19:56 -0300

    relatorio por usuario

--- front_sistemas_hoteis_gp2/src/App.jsx
+++ front_sistemas_hoteis_gp2/src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
+import { Link, Routes, Route } from "react-router-dom";
 import CancelarReserva from "./reserva/cancelarReserva";
 import CadastrarReserva from "./reserva/cadastrarReserva";
 import ListarReservas from "./reserva/listarReserva";
+import RelatorioUsuario from "./usuario/relatorioUsuario";
 
 function App() {
   return (
@@ -9,11 +11,13 @@ function App() {
       <Link to="/cadastrarReserva">Cadastrar Reserva</Link>
       <Link to="/listarReservas">Listar Reservas</Link>
       <Link to="/cancelarReserva">Remover Reserva</Link>
+      <Link to="/relatorioUsuario">Relatório de Usuário</Link>
       <br />
       <Routes>
         <Route path="/cadastrarReserva" element={<CadastrarReserva />} />
         <Route path="/listarReservas" element={<ListarReservas />} />
         <Route path="/cancelarReserva" element={<CancelarReserva />} />
+        <Route path="/relatorioUsuario" element={<RelatorioUsuario />} />
       </Routes>
     </>
   );
