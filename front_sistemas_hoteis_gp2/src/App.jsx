91dc6b039f6603578ca4f2e8b95d26b9da370b98
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-10-31 19:58:57 -0300

    Deixando um pouco mais organizado

--- front_sistemas_hoteis_gp2/src/App.jsx
+++ front_sistemas_hoteis_gp2/src/App.jsx
@@ -12,25 +12,20 @@ function App() {
   return (
     <>
 
-          <Link to="/listarUsuarios">Listar Usuários</Link> 
-        <br></br>
-        <Link to="/cadastrarUsuario">Cadastrar Usuário</Link>
-            <br></br>
-      <Link to="/cadastrarReserva">Cadastrar Reserva</Link>
-      <br></br>
-      <Link to="/listarReservas">Listar Reservas</Link>
-      <br></br>
-      <Link to="/cancelarReserva">Remover Reserva</Link>
-      <br></br>
-      <Link to="/relatorioUsuario">Relatório de Usuário</Link>
-      <br />
+      <Link to="/listarUsuarios">Listar Usuários</Link><br/>
+      <Link to="/cadastrarUsuario">Cadastrar Usuário</Link><br/>
+      <Link to="/cadastrarReserva">Cadastrar Reserva</Link><br/>
+      <Link to="/listarReservas">Listar Reservas</Link><br/>
+      <Link to="/cancelarReserva">Remover Reserva</Link><br/>
+      <Link to="/relatorioUsuario">Relatório de Usuário</Link><br/>
+
       <Routes>
-          <Route path="/cadastrarUsuario" element={<UserRegister />} />
-        <Route path="/listarUsuarios" element={<UserList />} />
-        <Route path="/cadastrarReserva" element={<CadastrarReserva />} />
-        <Route path="/listarReservas" element={<ListarReservas />} />
-        <Route path="/cancelarReserva" element={<CancelarReserva />} />
-        <Route path="/relatorioUsuario" element={<RelatorioUsuario />} />
+          <Route path="/cadastrarUsuario" element={<UserRegister/>}></Route>
+        <Route path="/listarUsuarios" element={<UserList/>}></Route>
+        <Route path="/cadastrarReserva" element={<CadastrarReserva/>}></Route>
+        <Route path="/listarReservas" element={<ListarReservas/>}></Route>
+        <Route path="/cancelarReserva" element={<CancelarReserva/>}></Route>
+        <Route path="/relatorioUsuario" element={<RelatorioUsuario/>}></Route>
       </Routes>
     </>
   );
